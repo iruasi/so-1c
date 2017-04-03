06641c93f88805448ece398daff7960f4ab6f68a
iña <isauri.github@goat.si>
2017-04-03 18:36:21 -0300

    Se agrega el comportamiento necesario para conectar con un IP y puertos definidos

---
+++ errorCodes.h
@@ -0,0 +1,7 @@
+/* Propongo algunos error codes para poder guiarnos en el debug del proceso,
+ * despues podemos ir ajustando los valores de estas cosas y demas...
+ * El TP se reserva los errores -1 al -9 y el -20;
+ * una terminacion correcta es 0 y los valores positivos no se permiten usar.
+ */
+
+#define ERR_FUNC_NATIVA -21
