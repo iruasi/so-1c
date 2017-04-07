b677afacca5180177210fed0d8b7102fac72ff86
iña <isauri.github@goat.si>
2017-04-07 19:46:23 -0300

    Se agrega funcionalidad en bruto al proceso Kernel. Se eliminan archivos basura..

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -1,8 +1,61 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
 
-int main(){
-	//Proyecto para subir
-	printf("Proceso KERNEL\n");
-	return EXIT_SUCCESS;
+#define PUERTO_INCOME "5010"
+#define MAXCLIENTS 5
+
+#define MAXMSJ 100
+
+void setupHints(struct addrinfo *hint, int address_family, int socket_type, int flags);
+int recibirConexion();
+
+int recibirConexion(){
+
+	char buf[MAXMSJ];
+
+	int stat;
+	int sock_listen;
+	struct addrinfo hints, *serverInfo;
+
+	int sock_cli;
+	struct sockaddr_in clientInfo;
+	socklen_t clientSize = sizeof(clientInfo);
+
+	setupHints(&hints, AF_UNSPEC, SOCK_STREAM, AI_PASSIVE);
+
+	if ((stat = getaddrinfo(NULL, PUERTO_INCOME, &hints, &serverInfo)) != 0){
+		fprintf("getaddrinfo: %s\n", gai_strerror(stat));
+		return -21;
+	}
+
+	sock_listen = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+
+	bind(sock_listen, serverInfo->ai_addr, serverInfo->ai_addrlen);
+	freeaddrinfo(serverInfo);
+
+	listen(sock_listen, MAXCLIENTS); // syscall bloqueante
+	sock_cli = accept(sock_listen, (struct sockaddr *) &clientInfo, &clientSize);
+
+	recv(sock_cli, buf, MAXMSJ, 0);
+	printf ("%s\n", buf);
+
+	return 0;
+}
+
+int main(void){
+
+	int stat;
+
+	stat = recibirConexion();
+
+	return stat;
+}
+
+void setupHints(struct addrinfo *hint, int fam, int sockType, int flags){
+
+	memset(hint, 0, sizeof *hint);
+	hint->ai_family = fam;
+	hint->ai_socktype = sockType;
+	hint->ai_flags = flags;
 }
