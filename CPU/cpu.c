276c8eac81130c29b26bb4b6feef9e523b37eccb
marrtins <martinsilva.1995@gmail.com>
2017-04-10 11:44:00 -0300

    saque los proyectos mal creados

--- CPU/cpu.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(){
-	//Proyecto para subir
-	printf("Proceso CPU\n");
-	return EXIT_SUCCESS;
-}
--- FileSystem/fileSystem.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(){
-	//Proyecto para subir
-	printf("proceso FILE SYSTEM\n");
-	return EXIT_SUCCESS;
-}
