cba9d5a41bcac4a292fba3ba8b31f814cb373a42
iña <isauri.github@goat.si>
2017-04-11 22:42:34 -0300

    Se modifica el proceso kernel en varios aspectos.
    La estructura tKernel ahora utiliza char* para puertos e IPs en vez de integers; Se agrega el soporte necesario de mallocs para que no haga segfaults; Se agrega procedimiento para mostrar por pantalla la configuracion de Kernel; Se agrega procedimiento para liberar los recursos del tKernel que usaba los mallocs

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
 #include <commons/config.h>
@@ -8,15 +8,55 @@
 #include <commons/collections/list.h>
 #include "kernel.h"
 
-#define PUERTO_INCOME "5010"
-#define MAXCLIENTS 5
+#define BACKLOG 5
 
+#define MAX_IP_LEN 16   // aaa.bbb.ccc.ddd -> son 15 caracteres, 16 contando un '\0'
+#define MAX_PORT_LEN 6  // 65535 -> 5 digitos, 6 contando un '\0'
 #define MAXMSJ 100
 
-void setupHints(struct addrinfo *hint, int address_family, int socket_type, int flags);
-int recibirConexion();
+#define FALLO_GRAL -21
+#define FALLO_CONFIGURACION -22
 
-int recibirConexion(){
+void setupHints(struct addrinfo *hint, int address_family, int socket_type, int flags);
+int recibirConexion(char *puerto_de_comunicacion);
+
+void mostrarConfiguracion(tKernel *datos_kernel);
+void liberarConfiguracionKernel(tKernel *datos_kernel);
+
+int main(int argc, char* argv[]){
+	if(argc!=2){
+			printf("Error en la cantidad de parametros\n");
+			return EXIT_FAILURE;
+	}
+
+	tKernel *kernel = getConfigKernel(argv[1]);
+
+	int stat;
+
+	mostrarConfiguracion(kernel);
+
+//	stat = recibirConexion(kernel->puerto_memoria); todavia no existe el proceso memoria
+
+//	stat = recibirConexion(kernel->puerto_fs); todavia no existe el proceso filesystem
+
+	stat = recibirConexion(kernel->puerto_prog);
+	printf("stat es: %d\n", stat);
+	stat = recibirConexion(kernel->puerto_cpu);
+	printf("stat es: %d\n", stat);
+
+	liberarConfiguracionKernel(kernel);
+	return stat;
+}
+
+void setupHints(struct addrinfo *hint, int fam, int sockType, int flags){
+
+	memset(hint, 0, sizeof *hint);
+	hint->ai_family = fam;
+	hint->ai_socktype = sockType;
+	hint->ai_flags = flags;
+}
+
+int recibirConexion(char *port_comm){
 
 	char buf[MAXMSJ];
 
@@ -30,9 +70,9 @@ int recibirConexion(){
 
 	setupHints(&hints, AF_UNSPEC, SOCK_STREAM, AI_PASSIVE);
 
-	if ((stat = getaddrinfo(NULL, PUERTO_INCOME, &hints, &serverInfo)) != 0){
+	if ((stat = getaddrinfo(NULL, port_comm, &hints, &serverInfo)) != 0){
 		fprintf("getaddrinfo: %s\n", gai_strerror(stat));
-		return -21;
+		return FALLO_GRAL;
 	}
 
 	sock_listen = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
@@ -40,7 +80,7 @@ int recibirConexion(){
 	bind(sock_listen, serverInfo->ai_addr, serverInfo->ai_addrlen);
 	freeaddrinfo(serverInfo);
 
-	listen(sock_listen, MAXCLIENTS); // syscall bloqueante
+	listen(sock_listen, BACKLOG); // syscall bloqueante
 	sock_cli = accept(sock_listen, (struct sockaddr *) &clientInfo, &clientSize);
 
 	recv(sock_cli, buf, MAXMSJ, 0);
@@ -49,86 +89,71 @@ int recibirConexion(){
 	return 0;
 }
 
-int main(int argc, char* argv[]){
-	if(argc!=2){
-			printf("Error en la cantidad de parametros\n");
-			return EXIT_FAILURE;
-	}
-	tKernel *kernel = getConfigKernel(argv[1]);
-	int stat;
-
-	stat = recibirConexion();
-
-	return stat;
-}
-
-void setupHints(struct addrinfo *hint, int fam, int sockType, int flags){
-
-	memset(hint, 0, sizeof *hint);
-	hint->ai_family = fam;
-	hint->ai_socktype = sockType;
-	hint->ai_flags = flags;
-}
-
 tKernel *getConfigKernel(char* ruta){
+
 	printf("Ruta del archivo de configuracion: %s\n", ruta);
 	tKernel *kernel = malloc(sizeof(tKernel));
+
+	kernel->algoritmo   = malloc(2 * sizeof kernel->algoritmo);
+	kernel->ip_fs       = malloc(MAX_IP_LEN * sizeof kernel->ip_fs);
+	kernel->ip_memoria  = malloc(MAX_IP_LEN * sizeof kernel->ip_memoria);
+	kernel->puerto_cpu  = malloc(MAX_PORT_LEN * sizeof kernel->puerto_cpu);
+	kernel->puerto_prog = malloc(MAX_PORT_LEN * sizeof kernel->puerto_prog);
+	kernel->puerto_memoria = malloc(MAX_PORT_LEN * sizeof kernel->puerto_memoria);
+	kernel->puerto_fs   = malloc(MAX_PORT_LEN * sizeof kernel->puerto_fs);
+
 	t_config *kernelConfig = config_create(ruta);
-	kernel->algoritmo = config_get_string_value(kernelConfig, "ALGORITMO");
+
+	strcpy(kernel->algoritmo,  config_get_string_value(kernelConfig, "ALGORITMO"));
+	strcpy(kernel->ip_fs,      config_get_string_value(kernelConfig, "IP_FS"));
+	strcpy(kernel->ip_memoria, config_get_string_value(kernelConfig, "IP_MEMORIA"));
+	strcpy(kernel->puerto_prog,    config_get_string_value(kernelConfig, "PUERTO_PROG"));
+	strcpy(kernel->puerto_cpu, config_get_string_value(kernelConfig, "PUERTO_CPU"));
+	strcpy(kernel->puerto_memoria, config_get_string_value(kernelConfig, "PUERTO_MEMORIA"));
+	strcpy(kernel->puerto_fs,  config_get_string_value(kernelConfig, "PUERTO_FS"));
+
 	kernel->grado_multiprog = config_get_int_value(kernelConfig, "GRADO_MULTIPROG");
-	kernel->ip_fs = config_get_string_value(kernelConfig, "IP_FS");
-	kernel->ip_memoria = config_get_string_value(kernelConfig, "IP_MEMORIA");
-	kernel->puerto_cpu = config_get_int_value(kernelConfig, "PUERTO_CPU");
-	kernel->puerto_memoria = config_get_int_value(kernelConfig, "PUERTO_MEMORIA");
-	kernel->puerto_prog = config_get_int_value(kernelConfig, "PUERTO_PROG");
-	kernel->quantum = config_get_int_value(kernelConfig, "QUANTUM");
-	kernel->quantum_sleep = config_get_int_value(kernelConfig, "QUANTUM_SLEEP");
-	kernel->stack_size = config_get_int_value(kernelConfig, "STACK_SIZE");
-	kernel->sem_ids = config_get_array_value(kernelConfig, "SEM_IDS");
-	kernel->sem_init = config_get_array_value(kernelConfig, "SEM_INIT");
-	kernel->shared_vars = config_get_array_value(kernelConfig, "SHARED_VARS");
+	kernel->quantum         = config_get_int_value(kernelConfig, "QUANTUM");
+	kernel->quantum_sleep   = config_get_int_value(kernelConfig, "QUANTUM_SLEEP");
+	kernel->stack_size      = config_get_int_value(kernelConfig, "STACK_SIZE");
+	kernel->sem_ids         = config_get_array_value(kernelConfig, "SEM_IDS");
+	kernel->sem_init        = config_get_array_value(kernelConfig, "SEM_INIT");
+	kernel->shared_vars     = config_get_array_value(kernelConfig, "SHARED_VARS");
+
+	config_destroy(kernelConfig);
+	return kernel;
+
+}
+
+
+// todo: hacer bien el recorrido de vectores
+void mostrarConfiguracion(tKernel *kernel){
 
 	printf("Algoritmo: %s\n", kernel->algoritmo);
 	printf("Grado de multiprogramacion: %d\n", kernel->grado_multiprog);
 	printf("IP para el File System: %s\n", kernel->ip_fs);
 	printf("IP para la memoria: %s\n", kernel->ip_memoria);
-	printf("Puerto CPU: %d\n", kernel->puerto_cpu);
-	printf("Puerto para la memoria: %d\n", kernel->puerto_memoria);
-	printf("Puerto para los programas: %d\n", kernel->puerto_prog);
+	printf("Puerto CPU: %s\n", kernel->puerto_cpu);
+	printf("Puerto para la memoria: %s\n", kernel->puerto_memoria);
+	printf("Puerto para los programas: %s\n", kernel->puerto_prog);
+	printf("Puerto para el filesystem: %s\n", kernel->puerto_fs);
 	printf("Quantum: %d\n", kernel->quantum);
 	printf("Quantum Sleep: %d\n", kernel->quantum_sleep);
 	printf("Stack size: %d\n", kernel->stack_size);
-	printf("Identificadores de semaforos: ");
-	//HABRIA QUE HACER UNA FUNCION DESDE ACA
-	int x=0;
-	while(kernel->sem_ids[x]){
-		if (kernel->sem_ids[x+1] == NULL){
-				kernel->sem_ids[x][strlen(kernel->sem_ids[x])-1]='\0';
-			}
-		printf("%s ", kernel->sem_ids[x]);
-		x++;
-	}
-	//HASTA ACA
-	x=0;
-	printf("\nValores iniciales de cada semaforo:");
-	while(kernel->sem_init[x]){
-		if (kernel->sem_init[x+1] == NULL){
-						kernel->sem_init[x][strlen(kernel->sem_init[x])-1]='\0';
-		}
-		printf("%s ", kernel->sem_init[x]);
-		x++;
-	}
-	printf("\nVariables compartidas: ");
-	x=0;
-	while(kernel->shared_vars[x]){
-		if (kernel->shared_vars[x+1] == NULL){
-				kernel->shared_vars[x][strlen(kernel->shared_vars[x])-1]='\0';
-			}
-		printf("%s ", kernel->shared_vars[x]);
-		x++;
-	}
+	printf("Identificadores de semaforos: %s, %s, %s\n", kernel->sem_ids[0], kernel->sem_ids[1], kernel->sem_ids[2]);
+	printf("Valores de semaforos: %s, %s, %s\n", kernel->sem_init[0], kernel->sem_init[1], kernel->sem_init[2]);
+	printf("Variables globales: %s, %s, %s\n", kernel->shared_vars[0], kernel->shared_vars[1], kernel->shared_vars[2]);
 
-	config_destroy(kernelConfig);
-	return kernel;
+}
 
+void liberarConfiguracionKernel(tKernel *kernel){
+
+	free(kernel->algoritmo);
+	free(kernel->ip_fs);
+	free(kernel->ip_memoria);
+	free(kernel->puerto_cpu);
+	free(kernel->puerto_memoria);
+	free(kernel->puerto_prog);
+	free(kernel->puerto_fs);
+	free(kernel);
 }
