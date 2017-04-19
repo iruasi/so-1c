bf6177f30ffd3475efc043c9c2f205a7b76f17d4
Rodrigo <rodillob9@gmail.com>
2017-04-19 00:20:51 -0300

    creación libreria funcionesComunes.h
    La libreria contiene funciones que se implementan en dos o mas de los elementos, resta implementar la libreria en los distintos archivos y eliminar la definición de prototipos y descripción de funciones que ahora se hallan en esta libreria

---
+++ funcionesComunes.h
@@ -0,0 +1,65 @@
+void setupHints(struct addrinfo *hints, int address_family, int socket_type, int flags){
+  memset(hint, 0, sizeof *hint);
+	hint->ai_family = fam;
+	hint->ai_socktype = sockType;
+	hint->ai_flags = flags;
+}
+
+
+int establecerConexion(char *ip_dest, char *port_dest){
+
+	int stat;
+	int sock_dest; // file descriptor para el socket del destino a conectar
+	struct addrinfo hints, *destInfo;
+
+	setupHints(&hints, AF_UNSPEC, SOCK_STREAM, 0);
+
+	if ((stat = getaddrinfo(ip_dest, port_dest, &hints, &destInfo)) != 0){
+		fprintf("getaddrinfo: %s\n", gai_strerror(stat));
+		return FALLO_GRAL;
+	}
+
+	if ((sock_dest = socket(destInfo->ai_family, destInfo->ai_socktype, destInfo->ai_protocol)) < 0)
+		return FALLO_GRAL;
+
+	connect(sock_dest, destInfo->ai_addr, destInfo->ai_addrlen);
+	freeaddrinfo(destInfo);
+
+	if (sock_dest < 0){
+		printf("Error al tratar de conectar con Kernel!");
+		return FALLO_CONEXION;
+	}
+
+	return sock_dest;
+}
+
+
+int makeListenSock(char *port_listen){
+
+	int stat, sock_listen;
+
+	struct addrinfo hints, *serverInfo;
+
+	setupHints(&hints, AF_UNSPEC, SOCK_STREAM, AI_PASSIVE);
+
+	if ((stat = getaddrinfo(NULL, port_listen, &hints, &serverInfo)) != 0){
+		fprintf("getaddrinfo: %s\n", gai_strerror(stat));
+		return FALLO_GRAL;
+	}
+
+	sock_listen = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+	bind(sock_listen, serverInfo->ai_addr, serverInfo->ai_addrlen);
+
+	freeaddrinfo(serverInfo);
+	return sock_listen;
+}
+
+int makeCommSock(int socket_in){
+
+	struct sockaddr_in clientAddr;
+	socklen_t clientSize = sizeof(clientAddr);
+
+	int sock_comm = accept(socket_in, (struct sockaddr*) &clientAddr, &clientSize);
+
+	return sock_comm;
+}
