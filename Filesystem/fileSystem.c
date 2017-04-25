58da322f2fe8b2356d931ec8da18c11a51968e49
iña <isauri.github@goat.si>
2017-04-25 01:11:53 -0300

    Mantenimiento en Filesystem: limpieza y acutalizacion de codigo.
    Se reemplazan las funciones redefinidas por aquellas en
    funcionesCompartidas, se limpian variables inutilizadas.

--- Filesystem/fileSystem.c
+++ Filesystem/fileSystem.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fileSystem.h"
+#include <string.h>
+#include <netdb.h>
+#include <unistd.h>
 #include <commons/config.h>
 #include <commons/string.h>
 #include <commons/log.h>
 #include <commons/collections/list.h>
-#include <string.h>
-#include <netdb.h>
+
+#include "../Compartidas/funcionesCompartidas.c"
+#include "../Compartidas/tiposErrores.h"
+#include "fileSystem.h"
 
 #define BACKLOG 10
-
-int establecerConexion(char *ip_destino, char *puerto_destino);
-int setSocketComm(char *puerto_de_comunicacion);
 
 
 int main(int argc, char* argv[]){
@@ -22,10 +23,7 @@ int main(int argc, char* argv[]){
 	}
 
 	char buf[MAXMSJ];
-	int i;
 	int stat;
-	int sock_kern;
-	int bytes_sent;
 
 	tFileSystem* fileSystem = getConfigFS(argv[1]);
 	mostrarConfiguracion(fileSystem);
@@ -33,110 +31,46 @@ int main(int argc, char* argv[]){
 	//SV SIMPLE PARA QUE SE LE CONECTE KERNEL
 
 	// Creamos sockets listos para send/recv para cada proceso
-	int fd_kernel = setSocketComm(fileSystem->puerto);
+	int sock_lis_kern = makeListenSock(fileSystem->puerto_entrada);
+	listen(sock_lis_kern, BACKLOG);
+	int sock_kern = makeCommSock(sock_lis_kern);
 
 
-	while ((stat = recv(fd_kernel, buf, MAXMSJ, 0)) > 0){
+	while ((stat = recv(sock_kern, buf, MAXMSJ, 0)) > 0){
 
 		printf("%s\n", buf);
+		clearBuffer(buf, MAXMSJ);
+	}
 
-		for (i = 0; i < MAXMSJ; ++i)
-			buf[i] = '\0';
-		}
-
-		if (bytes_sent == -1){
-			printf("Error en la recepcion de datos!\n valor retorno recv: %d \n", bytes_sent);
-			return FALLO_RECV;
-		}
+	if (stat < 0){
+		printf("Error en la recepcion de datos!\n valor retorno recv: %d \n", stat);
+		return FALLO_RECV;
+	}
 
 
 	//FIN SV SIMPLE
 
-
-
-
-
-
-
-
-
-	/*printf("Conectando con kernel...\n");
-	sock_kern = establecerConexion(fileSystem->ip_kernel, fileSystem->puerto);
-	if (sock_kern < 0)
-		return sock_kern;
-
-	while((stat = recv(sock_kern, buf, MAXMSJ, 0)) > 0){
-		printf("%s\n", buf);
-
-		for(i = 0; i < MAXMSJ; ++i)
-			buf[i] = '\0';
-	}
-
-	if (stat == -1){
-		printf("Error en la conexion con Kernel! status: %d \n", stat);
-		return -1;
-	}
-
 	printf("Kernel termino la conexion\nLimpiando proceso...\n");
-
-	close(sock_kern);*/
+	close(sock_kern);
 	liberarConfiguracionFileSystem(fileSystem);
 	return EXIT_SUCCESS;
 }
 
-void setupHints(struct addrinfo *hint, int fam, int sockType, int flags){
 
-	memset(hint, 0, sizeof *hint);
-	hint->ai_family = fam;
-	hint->ai_socktype = sockType;
-	hint->ai_flags = flags;
-}
-
-
-/* Dados un ip y puerto de destino, se crea, conecta y retorna socket apto para comunicacion
- */
-int establecerConexion(char *ip_dest, char *port_dest){
-
-	int stat;
-	int sock_dest; // file descriptor para el socket del destino a conectar
-	struct addrinfo hints, *destInfo;
-
-	setupHints(&hints, AF_UNSPEC, SOCK_STREAM, 0);
-
-	if ((stat = getaddrinfo(ip_dest, port_dest, &hints, &destInfo)) != 0){
-		fprintf("getaddrinfo: %s\n", gai_strerror(stat));
-		return FALLO_GRAL;
-	}
-
-	if ((sock_dest = socket(destInfo->ai_family, destInfo->ai_socktype, destInfo->ai_protocol)) < 0)
-		return FALLO_GRAL;
-
-	connect(sock_dest, destInfo->ai_addr, destInfo->ai_addrlen);
-	freeaddrinfo(destInfo);
-
-	if (sock_dest < 0){
-		printf("Error al tratar de conectar con Kernel!");
-		return FALLO_CONEXION;
-	}
-
-	return sock_dest;
-}
 
 tFileSystem* getConfigFS(char* ruta){
 	printf("Ruta del archivo de configuracion: %s\n", ruta);
 	tFileSystem *fileSystem= malloc(sizeof(tFileSystem));
 
-	fileSystem->puerto        = malloc(MAX_PORT_LEN * sizeof(fileSystem->puerto));
-	fileSystem->punto_montaje = malloc(MAX_IP_LEN * sizeof(fileSystem->punto_montaje));
-	fileSystem->ip_kernel     = malloc(MAX_IP_LEN * sizeof(fileSystem->ip_kernel));
-	//fileSystem->puerto_kernel = malloc(MAX_PORT_LEN * sizeof(fileSystem->puerto_kernel));
+	fileSystem->puerto_entrada = malloc(MAX_PORT_LEN);
+	fileSystem->punto_montaje  = malloc(MAX_IP_LEN);
+	fileSystem->ip_kernel      = malloc(MAX_IP_LEN);
 
 	t_config *fileSystemConfig = config_create(ruta);
 
-	strcpy(fileSystem->puerto, config_get_string_value(fileSystemConfig, "PUERTO"));
-	//strcpy(fileSystem->puerto_kernel, config_get_string_value(fileSystemConfig, "PUERTO_KERNEL"));
-	strcpy(fileSystem->punto_montaje, config_get_string_value(fileSystemConfig, "PUNTO_MONTAJE"));
-	strcpy(fileSystem->ip_kernel, config_get_string_value(fileSystemConfig, "IP_KERNEL"));
+	strcpy(fileSystem->puerto_entrada, config_get_string_value(fileSystemConfig, "PUERTO_ENTRADA"));
+	strcpy(fileSystem->punto_montaje,  config_get_string_value(fileSystemConfig, "PUNTO_MONTAJE"));
+	strcpy(fileSystem->ip_kernel,      config_get_string_value(fileSystemConfig, "IP_KERNEL"));
 
 	config_destroy(fileSystemConfig);
 	return fileSystem;
@@ -144,48 +78,15 @@ tFileSystem* getConfigFS(char* ruta){
 
 void mostrarConfiguracion(tFileSystem *fileSystem){
 
-	printf("Puerto: %s\n", fileSystem->puerto);
+	printf("Puerto: %s\n",           fileSystem->puerto_entrada);
 	printf("Punto de montaje: %s\n", fileSystem->punto_montaje);
-	printf("IP del kernel: %s\n", fileSystem->ip_kernel);
-	//printf("Puerto del kernel: %s\n", fileSystem->puerto_kernel);
-
+	printf("IP del kernel: %s\n",    fileSystem->ip_kernel);
 }
 
 void liberarConfiguracionFileSystem(tFileSystem *fileSystem){
 
 	free(fileSystem->punto_montaje);
 	free(fileSystem->ip_kernel);
-	free(fileSystem->puerto);
+	free(fileSystem->puerto_entrada);
 	free(fileSystem);
-}
-
-int setSocketComm(char *port_comm){
-        int stat;
-        int sock_listen;
-        struct addrinfo hints, *serverInfo;
-
-        int sock_comm;
-        struct sockaddr_in clientInfo;
-        socklen_t clientSize = sizeof(clientInfo);
-
-        setupHints(&hints, AF_UNSPEC, SOCK_STREAM, AI_PASSIVE);
-
-        if ((stat = getaddrinfo(NULL, port_comm, &hints, &serverInfo)) != 0){
-                fprintf("getaddrinfo: %s\n", gai_strerror(stat));
-                return FALLO_GRAL;
-        }
-
-        sock_listen = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
-
-        bind(sock_listen, serverInfo->ai_addr, serverInfo->ai_addrlen);
-        freeaddrinfo(serverInfo);
-
-        listen(sock_listen, BACKLOG);
-        printf("Estoy escuchando...\n");
-        sock_comm = accept(sock_listen, (struct sockaddr *) &clientInfo, &clientSize);
-        printf("Devuelvo un sock_comm: %d, para el del puerto %s\n", sock_comm, port_comm);
-
-        close(sock_listen);
-
-        return sock_comm;
 }
--- Filesystem/fileSystem.h
+++ Filesystem/fileSystem.h
@@ -9,14 +9,10 @@
 #define MAX_IP_LEN 16   // aaa.bbb.ccc.ddd -> son 15 caracteres, 16 contando un '\0'
 #define MAX_PORT_LEN 6  // 65535 -> 5 digitos, 6 contando un '\0'
 #define MAXMSJ 100
-
-#define FALLO_GRAL -21
-#define FALLO_CONFIGURACION -22
-#define FALLO_CONEXION -25
-#define FALLO_RECV -23
+#include "../Compartidas/tiposErrores.h"
 
 typedef struct{
-	char* puerto;
+	char* puerto_entrada;
 	char* punto_montaje;
 	char* ip_kernel;
 }tFileSystem;
